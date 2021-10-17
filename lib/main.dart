da6b364a4058ec3cac7ed57990465ed0ff01c838
NCM <neenuchacko@live.in>
2021-10-17 21:43:38 +0530

    MediaQuery issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main()=> runApp(MaterialApp(home:Homepage(),));
+import 'package:libman/screens/auth/login.dart';
 
-class Homepage extends StatefulWidget {
+// void main()=> runApp(MaterialApp(home:Homepage(),));
+
+// class Homepage extends StatefulWidget {
   
-  @override
-  _HomepageState createState() => _HomepageState();
-}
+//   @override
+//   _HomepageState createState() => _HomepageState();
+// }
 
-class _HomepageState extends State<Homepage> {
+// class _HomepageState extends State<Homepage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar:AppBar(
+//         backgroundColor:Colors.blue,
+//         title: Text('Sign In Page'),
+//       ),
+
+//     );
+//   }
+// }
+
+void main()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+ 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        backgroundColor:Colors.blue,
-        title: Text('Sign In Page'),
-      ),
+    final _init = Firebase.initializeApp();
+    return FutureBuilder(
+      future: _init,
+      builder:(BuildContext context, AsyncSnapshot snapshot){
+        if(snapshot.hasError){
+          return ErrorWidget();
+        }else if(snapshot.hasData){
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Login(),
+          );
+        }else{
+          return Loading();
+        }
+      },
 
     );
   }
 }
 
+class ErrorWidget extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children:[
+            Icon(Icons.error),
+            Text("Something went Wrong")
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Center(
+        child: CircularProgressIndicator(),
+
+      ),
+    );
+  }
+}
