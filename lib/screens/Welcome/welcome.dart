f08182a1c3c438be4d7210e76deac3b6f4872fca
Midhunkrishna k s <50834905+smidhunkks@users.noreply.github.com>
2021-11-04 14:47:10 +0530

    add login button

--- lib/screens/Welcome/welcome.dart
+++ lib/screens/Welcome/welcome.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:libman/Components/background.dart';
+import 'package:libman/constants.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -11,10 +14,14 @@ class WelcomeScreen extends StatelessWidget {
       body: Background(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          SizedBox(
+            height: size.height * .1,
+          ),
           Image.asset(
             "assets/images/splash_logo.png",
-            height: size.height * .25,
+            height: size.height * .21,
           ),
           const Text(
             "Libman",
@@ -23,11 +30,54 @@ class WelcomeScreen extends StatelessWidget {
                 fontSize: 50,
                 fontWeight: FontWeight.w400),
           ),
-          TextButton(
-              onPressed: () {
-                print("log in");
-              },
-              child: Text("Log in"))
+          Visibility(
+            visible: true,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomLeft,
+                        end: Alignment.topRight,
+                        colors: [kprimarycolor, kprimarylightcolor]),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  margin: EdgeInsets.only(
+                    left: size.width * .08,
+                    right: size.width * .08,
+                    top: size.height * .15,
+                  ),
+                  width: double.infinity,
+                  height: size.height * .08,
+                  child: TextButton(
+                      onPressed: () {
+                        print("Elevated button");
+                      },
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          print("signup");
+                        },
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w800, color: Colors.black),
+                        ))
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       )),
     );
