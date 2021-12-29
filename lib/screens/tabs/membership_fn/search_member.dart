c03054abe05b94ed55a143b7ebc996812aad773d
NCM <neenuchacko@live.in>
2021-12-29 11:46:51 +0530

    Search Member

--- lib/screens/tabs/membership_fn/search_member.dart
+++ lib/screens/tabs/membership_fn/search_member.dart
@@ -1,22 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:libman/Components/background.dart';
+import 'package:libman/Components/model/membership.dart';
 import 'package:libman/constants.dart';
+import 'package:intl/intl.dart';
+import 'package:libman/services/userservice.dart';
 
-class MemberSearch extends StatelessWidget {
-  const MemberSearch({Key? key}) : super(key: key);
+class MemberSearch extends StatefulWidget {
+  MemberSearch({Key? key}) : super(key: key);
+
+  @override
+  State<MemberSearch> createState() => _MemberSearchState();
+}
+
+class _MemberSearchState extends State<MemberSearch> {
+  final _searchformKey = GlobalKey<FormState>();
+  
+  TextEditingController name = TextEditingController();
+ 
+
+  
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Background(
-        child: SafeArea(
-          child: Column(
-            children: [
-              const Text(
-                "Search Member",
-                style: kscreentitle,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _searchformKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
               ),
-            ],
+              child: Column(
+                children: [
+                  const Text(
+                    "Search Member",
+                    style: kscreentitle,
+                  ),
+                  TextFormField(
+                    controller: name,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter Name";
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "Name",
+                      hintText: "Name",
+                    ),
+                  ),
+                 
+                  
+                ],
+              ),
+            ),
           ),
         ),
       ),
